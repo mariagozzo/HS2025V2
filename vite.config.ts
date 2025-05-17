e69d87ca422e2c91a21774588bca52a454521d4c
mariagozzo <mariagozzo@mail2one.com>
2025-05-16 21:39:28 -0400

    Fix Vite configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/HS2025V2/' : '/'
+  base: mode === 'production' ? '/HS2025V2/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
